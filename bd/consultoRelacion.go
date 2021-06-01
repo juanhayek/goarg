2b206c4164df9b6c3314e304f201d2f7be4e91eb
Juan Hayek <hayek17@gmail.com>
2021-05-31 19:03:18 -0500

    Ultimo nivel backend

---
+++ bd/consultoRelacion.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/juanhayek/goarg/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func ConsultoRelacion(t models.Relacion) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("goarg")
+	col := db.Collection("relacion")
+
+	condicion := bson.M{
+		"usuarioid":         t.UsuarioID,
+		"usuariorelacionid": t.UsuarioRelacionID,
+	}
+	fmt.Println(t.UsuarioID + " " + t.UsuarioRelacionID)
+	var resultado models.Relacion
+	err := col.FindOne(ctx, condicion).Decode(&resultado)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
